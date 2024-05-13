41827051cc1674fdf4245b6f32804e551ac65383
MuhammedAnasEP <anasep1110@gmail.com>
2024-05-13 20:02:37 +0530

    create the responsive navbar and insall the @headlessui/react for transition when it's small display(responsive)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import AddPatientForm from './components/AddPatientForm';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <AddPatientForm></AddPatientForm>
     </>
   );
